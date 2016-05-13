0f5222229ec73fc5ae109fcfa116d12438f1ba2a
Ivan Ponomarev <iponomarev@mirantis.com>
2016-05-13 16:09:11 -0500

    Fix OS_AUTH_URL to adapt to v3/v2.0 split
      Without this patch, openrc and env vars get passed directly through
      as-is without being modified, but OS_API_IDENTITY_VERSION was, causing
      breakage.

--- lib/puppet/provider/openstack/auth.rb
+++ lib/puppet/provider/openstack/auth.rb
@@ -3,6 +3,8 @@ require 'puppet/provider/openstack/credentials'
 module Puppet::Provider::Openstack::Auth
 
   RCFILENAME = "#{ENV['HOME']}/openrc"
+  #FIXME(mattymo): LP #1479763
+  RCFILENAME = '/root/openrc' unless File.exists?(RCFILENAME)
 
   def get_os_vars_from_env
     env = {}
